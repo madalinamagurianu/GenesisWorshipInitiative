cc41e6ee56588d2476c715421acc12dbf71f03d6
Madalina Magurianu <142689727+madalinamagurianu@users.noreply.github.com>
2025-08-13 18:34:01 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
+// assets/js/app.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+const url = window.__SUPABASE_URL__;
+const key = window.__SUPABASE_ANON_KEY__;
+export const supa = createClient(url, key);
+
 (() => {
   const url = window.__SUPABASE_URL__;
   const anon = window.__SUPABASE_ANON_KEY__;
